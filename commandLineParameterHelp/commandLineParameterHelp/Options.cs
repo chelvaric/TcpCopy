48a7d6dbd228c26847f5cc78eebde6c8a417dec0
chelvaric <willemswouter@hotmail.com>
2015-02-19 12:10:03 +0100

    command line parser testen
    testen was succesvoll

---
+++ commandLineParameterHelp/commandLineParameterHelp/Options.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commandLineParameterHelp
+{
+    class Options
+    {
+        [Option("ListenSocket", HelpText = "geef de socket op van de data tcp")]
+        public int ListenSocket { get; set; }
+       [Option("ListenIp", HelpText = "hier heeft u de ip in waar dat hij moet op luisteren")]
+        public string ListenIpAdress { get; set; }
+
+        [Option("sendSocket", HelpText = "socket waarop de data weer word uitgestuurd. shijd met ; voor meerdere")]
+        public string sendSocket { get; set; }
+
+        [Option('w', "wayofsending", HelpText = "true als u wilt sturen vergeet dan niet de SendIp en SendSocket in te stellen, false als u wilt laten conecteren u moet dan de sendsocket instellen naar waar u de data uitbrengt")]
+        public bool WayOfSend { get; set; }
+
+        [Option("SendIpAdress", HelpText = "ipadresses to send to, use ; to seperate them ")]
+        public string SendIp { get; set; }
+     
+    }
+}
---
+++ commandLineParameterHelp/commandLineParameterHelp/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using System.Net;
+
+namespace commandLineParameterHelp
+{
+    class Program
+    {
+       static List<string> _sendIpAdresses = new List<string>();
+        static List<string> _sendSockets = new List<string>();
+        static int _listenSocket;
+        static IPAddress _listenIpAdress;
+        static bool ServerOrClient;
+
+        static void Main(string[] args)
+        {
+            Options options = new Options();
+            var result = Parser.Default.ParseArguments(args,options);
+            if(Parser.Default.ParseArguments(args,options))
+            {
+               
+                if(options.WayOfSend)
+                {
+                    ServerOrClient = true;
+                    Console.WriteLine("ListenSocket: {0}", options.ListenSocket);
+                  Console.WriteLine("ListenIP: {0}", options.ListenIpAdress);
+                    Console.WriteLine("SendSocket or sockets:{0}", options.sendSocket);
+                    _listenSocket = options.ListenSocket;
+                    _listenIpAdress = IPAddress.Parse(options.ListenIpAdress);
+                    if(options.sendSocket.IndexOf(';') != -1)
+                    {
+
+                        String[] sepSock = options.sendSocket.Split(';');
+                        _sendIpAdresses.AddRange(sepSock);
+                    }
+                    else
+                    {
+                        _sendIpAdresses.Add(options.sendSocket);
+
+                    }
+
+
+                   
+                }
+                else
+                {
+                    ServerOrClient = false;
+                    Console.WriteLine("ListenSocket: {0}", options.ListenSocket);
+                    Console.WriteLine("ListenIP: {0}", options.ListenIpAdress);
+                    Console.WriteLine("SendSocket or sockets:{0}", options.sendSocket);
+                    Console.WriteLine("sendIps: {0}", options.SendIp);
+                    _listenIpAdress = IPAddress.Parse(options.ListenIpAdress);
+                    if (options.sendSocket.IndexOf(';') != -1)
+                    {
+
+                        String[] sepSock = options.sendSocket.Split(';');
+                        _sendSockets.AddRange(sepSock);
+                    }
+                    else
+                    {
+                        _sendSockets.Add(options.sendSocket);
+
+                    }
+
+                    if(options.SendIp.IndexOf(';') != -1)
+                    {
+                        String[] sepIp = options.SendIp.Split(';');
+                        _sendIpAdresses.AddRange(sepIp);
+                    }
+                    else
+                    {
+                        _sendIpAdresses.Add(options.SendIp);
+
+                    }
+                }
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
